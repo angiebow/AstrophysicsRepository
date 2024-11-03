95c92f98adafca297bdc9928f52fd4199bfc73db
Pelangi Masita <pelangimasita18@gmail.com>
2024-11-03 11:10:38 +0700

    frontend

---
+++ frontend/src/components/ResearchCard 2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="research-card">
+    <h3>{{ research.title }}</h3>
+    <p>{{ research.description }}</p>
+    <router-link :to="`/research/${research.id}`">Read more</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    research: Object,
+  },
+};
+</script>
+
+<style scoped>
+.research-card {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 5px;
+}
+</style>
